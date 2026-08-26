bcc15aa8b44a78554b84ecb3089ce0be5b9b6da8
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2026-08-25 22:28:33 -0300

    CREATED: Backend Project

---
+++ Backend/LojaProject/src/main/java/com/gabrielnz/lojaproject/LojaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielnz.lojaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojaProjectApplication.class, args);
+    }
+
+}
