1cac93c99e009931d9e08440a9b0e410e061fd4f
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2026-09-01 14:47:37 -0300

    IMPLEMENTED: Navbar with links working

--- Frontend/LojaProject/src/app/app.component.ts
+++ Frontend/LojaProject/src/app/app.component.ts
@@ -4,15 +4,18 @@ import { MenuItem } from 'primeng/api';
 import { Menubar } from 'primeng/menubar';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { CardModule } from 'primeng/card';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menubar, CommonModule],
+  imports: [RouterOutlet, Menubar, CommonModule, CardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  title = 'LojaProject';
+
   constructor(private router: Router) {}
 
   items: MenuItem[] | undefined;
